90b4a69e7cdb5da5b87ff356aebf1f7d50cad5d7
Wagner vicente <Wagner.vicentebr@gmail.com>
2021-03-17 17:42:09 -0300

    initial project

---
+++ src/controllers/emailController.js
@@ -0,0 +1,34 @@
+const sgMail= require('@sendgrid/mail')
+
+module.exports = {
+    async sendEmail(req, res) {
+    
+        const {to, subject, text, html} = req.body;
+        const API_KEY = req.headers.authorization;
+
+        sgMail.setApiKey(API_KEY)
+
+        const message = {
+            from: 'daniellost02@gmail.com',
+            to,
+            subject,
+            text,
+            html
+        }
+        
+        sgMail.send(message)
+            .then(response => console.log('email sent ....'))
+            .catch(error => console.log(error.message))
+
+        return res.json({ id: "funcionou" });
+    },
+    
+    hello(req, res) {
+        return res.json({ id: "funcionou" });
+    }
+}
+
+
+
+
+ 
